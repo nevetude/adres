401a26b53ab270ba0e49e240b35bef69f9655f8a
nevetude <nevetude@gmail.com>
2025-05-30 09:20:20 +0500

    Исправлено подключение к PostgreSQL для Render

--- db.js
+++ db.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: 'postgres', // Подключаемся к стандартной БД
-    password: process.env.DB_PASSWORD || 'postgres',
-    port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 const runQuery = async (query, params = []) => {
